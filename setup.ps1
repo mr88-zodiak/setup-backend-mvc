6afac16a0550e1afa43d49a78de62cc06c623759
mr88-zodiack <bimasopan14@gmail.com>
2025-08-09 15:38:19 +0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -19,19 +19,8 @@ Invoke-WebRequest -Uri "https://bit.ly/3Tfc9Tn" -OutFile "index.js"
 #Invoke-WebRequest -Uri "https://shorturl.at/QmDBV" -OutFile "view.zip"
 #Expand-Archive -Path "view.zip" -DestinationPath "view" -Force
 mkdir view
-cd view\view
+cd view\
 npm create vite@latest
-# $pilih = Read-Host "Mau install tailwindcss?[Y::N]"
-# if ($pilih.ToUpper() -eq "Y"){
-#     npm install -D tailwindcss
-#     npx tailwindcss init
-#     $ask = read-host "jalankan tailwindcss sekarang? [Y:N]"
-#     if ($ask.ToUpper() -eq "Y"){
-#          npx tailwindcss -i ./css/input.css -o ./css/style.css -- watch
-#     }
-# }
-cd .. 
-
 #del view.zip
 npm init -y
 
